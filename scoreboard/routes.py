8dd973583006d2d9f811d46da7903cd000104e75
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-07-14 12:58:11 +0200

    index route now goes to right controller method

--- scoreboard/routes.py
+++ scoreboard/routes.py
@@ -1,7 +1,8 @@
 from scoreboard import flask_app
+from controller import view_exercises
 
 
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
-    return "Hello, World!"
+    return view_exercises()
