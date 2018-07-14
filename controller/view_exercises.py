02289bd84579b08098f28a6ba2b8e435008c808e
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-07-14 16:16:25 +0200

    exercise objects correctly passed to jinja2

--- controller/view_exercises.py
+++ controller/view_exercises.py
@@ -1,10 +1,9 @@
 from flask import render_template
 from database.exercise import Exercise as Exercise
-from json import dumps
 
 
 def view_exercises():
     exercises = Exercise.query.all()
-    exercises = None if len(exercises) is 0 else dumps(exercises)
+    exercises = None if len(exercises) is 0 else exercises
 
     return render_template('exercises.html', exercises=exercises)
