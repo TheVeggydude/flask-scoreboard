079ab25e3da9d371866643c2cf9b5945ac4ec520
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-07-14 15:05:47 +0200

    oops

--- database/exercise.py
+++ database/exercise.py
@@ -4,6 +4,7 @@ from scoreboard import db
 class Exercise (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     index = db.Column(db.SmallInteger, nullable=False)
-    text = db.Column(db.String(1000), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.String(1000), nullable=False, default='')
     visible = db.Column(db.Boolean, default=False)
 
