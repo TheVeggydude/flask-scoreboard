8adc338d5ab6e8fd64e09678e94f4852345dd350
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-07-14 20:23:37 +0200

    scoreboard now shows ordered list of exercises

--- controller/view_exercises.py
+++ controller/view_exercises.py
@@ -3,7 +3,7 @@ from database.exercise import Exercise as Exercise
 
 
 def view_exercises():
-    exercises = Exercise.query.all()
+    exercises = Exercise.query.order_by(Exercise.index.asc()).all()
     exercises = None if len(exercises) is 0 else exercises
 
     return render_template('exercises.html', exercises=exercises)
