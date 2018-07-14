c35d143c6fe64e5646fabf4b68f7cc2a193b9b50
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-07-14 15:06:36 +0200

    `view_exercises()` now renders the correct template

--- controller/view_exercises.py
+++ controller/view_exercises.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from database.exercise import Exercise as Exercise
+from json import dumps
 
 
 def view_exercises():
     exercises = Exercise.query.all()
-    print(exercises)
+    exercises = None if len(exercises) is 0 else dumps(exercises)
+
+    return render_template('exercises.html', exercises=exercises)
