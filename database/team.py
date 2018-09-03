18bfc978611eabd234eebe5e958c74316468aa5d
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2018-09-03 15:22:30 +0200

    changed comment

--- database/team.py
+++ database/team.py
@@ -2,6 +2,9 @@ from scoreboard import db
 
 
 class Team (db.Model):
+    """
+    points: Just put a JSONified dict in here containing exercise number : points scored pairs
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    points = db.Column(db.String(150), nullable=False, default='')  # just put a JSONified dict in here
+    points = db.Column(db.String(150), nullable=False, default='')
